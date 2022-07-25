0a906b42d7fb9903971c0805f6ab3f014e7a2865
flutclass <flutclassgr1@gmail.com>
2022-07-25 09:30:26 +0430

    test1

---
+++ test1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 5;
+            for (int i = 1; i < length + 1; i++)
+            {
+                string result = "";
+                result = result + String.Concat(Enumerable.Repeat(" ", length - i));
+                for (int c = 1; c < i + 1; c++)
+                {
+                    result = result + c;
+                }
+                for (int j = i - 1; j > 0; j--)
+                {
+                    result = result + j;
+                }
+                result = result + String.Concat(Enumerable.Repeat(" ", length - i));
+                Console.WriteLine(result);
+            } 
+        }
+    }        
+}
+    
