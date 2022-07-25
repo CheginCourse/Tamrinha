fa34f57917953c7439d40fc49735361bcb5cc9ef
flutclass <flutclassgr1@gmail.com>
2022-07-25 09:42:35 +0430

    test3

---
+++ test3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str1;
+            string Str2;
+            Console.WriteLine("Enter string1:");
+            Str1 = Console.ReadLine();
+            Console.WriteLine("Enter string2:");
+            Str2 = Console.ReadLine();
+            for (int i = 0; i <= Str1.Length - Str2.Length; i++)
+            {
+                string subStr = Str1.Substring(i, Str2.Length);
+                if (subStr == Str2)
+                {
+                    Console.WriteLine("Index is " + i);
+                    break;
+                }
+            }
+            
+        }
+    }        
+}
+    
