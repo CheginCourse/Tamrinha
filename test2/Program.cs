60d68935fae5d0404935d3000f00272dac04bc02
flutclass <flutclassgr1@gmail.com>
2022-07-25 09:31:28 +0430

    test2

---
+++ test2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            int sum = 0;
+            int intVal;
+            Console.WriteLine("Enter the number:");
+            input = Console.ReadLine();
+            intVal = Convert.ToInt32(input);
+            for (int i = 1; i < intVal; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sum = sum + i;
+                }
+            }
+
+            Console.WriteLine("Sum is " + sum);
+        }
+    }        
+}
+    
