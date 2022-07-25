f2db546617fcd54a16e7fd0168d1ad272d0b9911
flutclass <flutclassgr1@gmail.com>
2022-07-25 09:44:13 +0430

    test6

---
+++ test6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string result = Binary(number);
+
+            Console.WriteLine("Result is " + result);
+        }
+        
+        public static string Binary(int number){
+            if (number/2 == 1)
+            {
+                return $"{number/2}{number%2}";
+            }
+
+           return  Binary(number/2) + $"{number%2}";
+        }
+    }
+}
+
