3ac632ca5f5f8f6a120cf6f0f1219d62d05d4701
flutclass <flutclassgr1@gmail.com>
2022-07-25 09:44:57 +0430

    test8

---
+++ test8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the word:");
+            string? word = Console.ReadLine();
+            string result = MakeWord(word, (word.Length/2), 0);
+             Console.WriteLine(result);
+            if(result == word){
+                Console.WriteLine("True");
+            }
+            else{
+                Console.WriteLine("False");
+            }
+        }
+
+
+        static string MakeWord(string word, int index, int counter){
+
+            if(counter >= index){
+                if(word.Length%2 == 0){
+                    return "";
+                }
+                else{
+                    return Convert.ToString(word[index]);
+                }
+            }
+            return $"{word[counter]}" + MakeWord(word, index, counter + 1) + $"{word[counter]}";
+           
+        }
+    }
+}
+
