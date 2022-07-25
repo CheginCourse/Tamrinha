81b79835cb27d68ab5642f3326f33d4bcb014d8c
flutclass <flutclassgr1@gmail.com>
2022-07-25 09:43:14 +0430

    test4

---
+++ test4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNum;
+            string Char;
+            int Repeat = 0;
+            Console.WriteLine("Enter big number:");
+            bigNum = Console.ReadLine();
+            Console.WriteLine("Enter character:");
+            Char = Console.ReadLine();
+            for (int i = 0; i < bigNum.Length; i++)
+            {
+                if (Char == Convert.ToString(bigNum[i]))
+                {
+                    Repeat = Repeat + 1;
+                }
+            }
+
+            Console.WriteLine("Repeat is " + Repeat);
+        }
+    }        
+}
+    
