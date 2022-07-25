d70cd5df0cb65df7891f143e7563f700f3347544
flutclass <flutclassgr1@gmail.com>
2022-07-25 09:43:51 +0430

    test5

---
+++ test5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int first = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            int result = Power(first, second);
+            Console.WriteLine("Result is" + result);
+
+        }
+        
+        public static int Power(int first,int second){
+
+            if (second == 1)
+            {
+                return first;
+            }
+
+
+            return first * Power(first, second-1);
+    
+        }
+    }
+}
+
